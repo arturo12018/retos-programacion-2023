fb79ac9a297e3a9708a1fb23cae38a8dd0d6b601
artur <artur@Arturo>
2023-04-14 21:18:35 -0600

    Juego tenis

--- RetoProgramacion2023/src/retoprogramacion2023/RetoProgramacion2023.java
+++ RetoProgramacion2023/src/retoprogramacion2023/RetoProgramacion2023.java
@@ -11,13 +11,23 @@ public class RetoProgramacion2023 {
     
      public static void main(String[] args){
          
+         /*
          //Prueba de "FIZZ BUZZ"
          FizzBuzz.solucion();
+         */
          
-         
+         /*
          //Prueba de Lenguaje Hacker
          System.out.println(LenguajeHacker.palabraHacker("Hola mundo"));
          System.out.println(LenguajeHacker.palabraHacker("Aquí está un texto de prueba para ver si funciona el reto!"));
+         */
+         
+         
+         //Prueba partido de tennis
+         
+         //String[] puntos={"P1","P1","P2","P1","P2","P2","P1","P2","P1","P2","P1","P1"};
+         String[] puntos={"P2","P1","P1","P2","P1","P2","P2","P1","P2","P1","P2","P2"};
+         PartidoTenis.partido(puntos);
      }
     
 }
