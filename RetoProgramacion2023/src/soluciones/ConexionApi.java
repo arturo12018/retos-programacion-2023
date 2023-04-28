160c0b3663e57a32e75f5e6fd5db21e79fb7c8b4
artur <artur@Arturo>
2023-04-27 19:51:53 -0600

    Conexion Api

--- RetoProgramacion2023/src/soluciones/ConexionApi.java
+++ RetoProgramacion2023/src/soluciones/ConexionApi.java
@@ -14,6 +14,19 @@ import java.net.URL;
  *
  * @author artur
  */
+
+
+/*
+ * Llamar a una API es una de las tareas más comunes en programación.
+ *
+ * Implementa una llamada HTTP a una API (la que tú quieras) y muestra su
+ * resultado a través de la terminal. Por ejemplo: Pokémon, Marvel...
+ *
+ * Aquí tienes un listado de posibles APIs: 
+ * https://github.com/public-apis/public-apis
+ */
+
+
 public class ConexionApi {
     public static void llamar(){
      try {
